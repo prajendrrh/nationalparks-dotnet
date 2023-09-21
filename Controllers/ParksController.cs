ac0f971f2cc94ea782b340629a4aa776a29ebe5c
prajendrrh <125906644+prajendrrh@users.noreply.github.com>
2023-09-21 12:33:18 +0100

    Update ParksController.cs

--- Controllers/ParksController.cs
+++ Controllers/ParksController.cs
@@ -42,7 +42,7 @@ namespace NationalParks.Controllers
             var jsonData = new
             {
                 id = "nationalparks",
-                displayName = "National Parks (C#)",
+                displayName = "Awesome National Parks (C#)",
                 center = new
                         {
                             latitude = 47.039304,
